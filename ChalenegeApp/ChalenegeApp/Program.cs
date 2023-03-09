e074201c3776b793342a57aeb3caa110b0c5d029
poplukasz <poplukasz@gmail.com>
2023-03-09 09:16:42 +0100

    Zadanie dzień 4 wyzwanie21

--- ChalenegeApp/ChalenegeApp/Program.cs
+++ ChalenegeApp/ChalenegeApp/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var name = "Lukasz";
+char plec = 'M';
+var wiek = 17;
+if (plec == 'K')
+{
+    if (wiek < 30)
+    {
+        Console.WriteLine("Kobieta poniżej 30 lat");
+    }
+    else if (name == "Ewa" && wiek == 33)
+    {
+        Console.WriteLine("Ewa lat 33");
+    }
+}
+else if (wiek < 18)
+{
+    Console.WriteLine("Mężczyzna niepełnoletni");
+}
+else
+{
+    Console.WriteLine("Mężczyzna lat " + wiek);
+}
