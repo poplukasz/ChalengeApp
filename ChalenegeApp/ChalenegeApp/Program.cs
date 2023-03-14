92c3352765e8416f25faee627fe25f250e8a3884
poplukasz <poplukasz@gmail.com>
2023-03-14 19:25:03 +0100

    Zadanie domowe dzień 5

--- ChalenegeApp/ChalenegeApp/Program.cs
+++ ChalenegeApp/ChalenegeApp/Program.cs
@@ -1,22 +1,101 @@
-﻿var name = "Lukasz";
-char plec = 'M';
-var wiek = 17;
-if (plec == 'K')
+﻿using System.Globalization;
+
+var number = 6764;
+string textnumber = number.ToString();
+char[] litera = textnumber.ToCharArray();
+int[] licznik = new int[10];
+licznik[0] = 0;
+licznik[1] = 0;
+licznik[2] = 0;
+licznik[3] = 0;
+licznik[4] = 0;
+licznik[5] = 0; 
+licznik[6] = 0;
+licznik[7] = 0;
+licznik[8] = 0;
+licznik[9] = 0;
+
+for (int i = 0; i < litera.Length; i++)
 {
-    if (wiek < 30)
+    int x = 0;
+    if (litera[i] == '0')
     {
-        Console.WriteLine("Kobieta poniżej 30 lat");
+        licznik[x]++;
     }
-    else if (name == "Ewa" && wiek == 33)
+    else
     {
-        Console.WriteLine("Ewa lat 33");
+        x++;
+        if (litera[i] == '1')
+        {
+            licznik[x]++;
+        }
+        else
+        {
+            x++;
+            if (litera[i] == '2')
+            {
+                licznik[x]++;
+            }
+            else
+            {
+                x++;
+                if (litera[i] == '3')
+                {
+                    licznik[x]++;
+                }
+                else
+                {
+                    x++;
+                    if (litera[i] == '4')
+                    {
+                        licznik[x]++;
+                    }
+                    else
+                    {
+                        x++;
+                        if (litera[i] == '5')
+                        {
+                            licznik[x]++;
+                        }
+                        else
+                        {
+                            x++;
+                            if (litera[i] == '6')
+                            {
+                                licznik[x]++;
+                            }
+                            else
+                            {
+                                x++;
+                                if (litera[i] == '7')
+                                {
+                                    licznik[x]++;
+                                }
+                                else
+                                {
+                                    x++;
+                                    if (litera[i] == '8')
+                                    {
+                                        licznik[x]++;
+                                    }
+                                    else
+                                    {
+                                        x++;
+                                        if (litera[i] == '9')
+                                        {
+                                            licznik[x]++;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
-else if (wiek < 18)
+for (int z = 0; z<10; z++)
 {
-    Console.WriteLine("Mężczyzna niepełnoletni");
-}
-else
-{
-    Console.WriteLine("Mężczyzna lat " + wiek);
+    Console.WriteLine(z + ">=" + licznik[z]);
 }
